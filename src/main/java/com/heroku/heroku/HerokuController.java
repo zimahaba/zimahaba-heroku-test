449e6376467e46ddcde4cc4fbb8c9ac670dfaeb6
zimahaba <pmaquitorres@gmail.com>
2020-05-04 20:17:10 -0300

    commit

---
+++ src/main/java/com/heroku/heroku/HerokuController.java
@@ -0,0 +1,13 @@
+package com.heroku.heroku;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HerokuController {
+
+    @GetMapping("/teste")
+    public String teste() {
+        return "OK";
+    }
+}
