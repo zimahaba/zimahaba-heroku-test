1bf9e5c6a2944bd0b0f9c5b370c65439bc75046b
zimahaba <pmaquitorres@gmail.com>
2020-05-04 20:38:32 -0300

    commit

---
+++ src/main/java/com/heroku/heroku/Heroku.java
@@ -0,0 +1,13 @@
+package com.heroku.heroku;
+
+public class Heroku {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- src/main/java/com/heroku/heroku/HerokuController.java
+++ src/main/java/com/heroku/heroku/HerokuController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HerokuController {
 
     @GetMapping("/teste")
-    public String teste() {
-        return "OPAAA";
+    public Heroku teste() {
+        Heroku h = new Heroku();
+        h.setTitle("OPS");
+        return h;
     }
 }
